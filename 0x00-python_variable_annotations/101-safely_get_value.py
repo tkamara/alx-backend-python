9704bd248523082c3a159c0c14ae859e2b3c809f
Chebet Kamara <chebetkamara@gmail.com>
2023-10-05 12:52:46 +0000

    Fixing linter error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,10 +4,11 @@ add type annotations to the function
 """
 from typing import Mapping, Any, Union, TypeVar
 
-
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) \
+        -> Union[Any, T]:
     """
     add type annotations to the function
     """
