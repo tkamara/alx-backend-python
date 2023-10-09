7a3fa16699a19c4da00415b96268420a4c8c8004
Chebet Kamara <chebetkamara@gmail.com>
2023-10-09 14:25:45 +0000

    Fixing errors

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@ multiple coroutines at the same time with async
 """
 import asyncio
 import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     return the list of all the delays
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,10 +4,11 @@ multiple coroutines at the same time with async
 """
 import asyncio
 import random
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> float:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     return the list of all the delays
     """
